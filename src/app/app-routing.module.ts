ce24a73c003470bed9eac50f14581f4ee25ec47b
amresh prasad <amresh04061999@gmail.com>
2023-06-16 08:17:40 +0530

    scss folder structure pennding

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'admin', loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule) }, { path: 'home-page', loadChildren: () => import('./home-page/home-page.module').then(m => m.HomePageModule) }, { path: 'registration', loadChildren: () => import('./registration/registration.module').then(m => m.RegistrationModule) }, { path: 'login', loadChildren: () => import('./login/login.module').then(m => m.LoginModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
